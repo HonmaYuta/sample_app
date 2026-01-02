93543faa5884e7188045a2045659590b8b63bb50
HonmaYuta <256octopus512@gmail.com>
2026-01-02 07:59:30 +0000

    finish 7-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,19 @@ class UsersController < ApplicationController
 
   def create
     # paramas => users => user.save => if ... else ... end
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new', status: :unprocessable_entity
     end
   end
+
+  private
+
+    def user_params
+      # Strong Parameters
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
